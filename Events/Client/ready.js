b2eb297608bc3e3cadf0d6fb705f8b9b07e6bb85
Carlos5965050 <149985178+Carlos5965050@users.noreply.github.com>
2023-11-11 12:21:26 -0600

    Update ready.js

--- Events/Client/ready.js
+++ Events/Client/ready.js
@@ -1,1 +1,15 @@
+const { loadCommands } = require("../../Handlers/commandHandler");
 
+module.exports = {
+
+name: "ready",
+
+once: true,
+
+execute(client) {
+
+console.log("El cliente ya esta listo");
+
+loadCommands(client);
+},
+};
